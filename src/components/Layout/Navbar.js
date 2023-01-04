ef3dc435069bfd42d36103048d82a58df3081511
KubaGlegola <kubaglegola@gmail.com>
2023-01-04 12:31:30 +0100

    fixed menu animation

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classes from "./Navbar.module.css";
 import { AiFillHome, AiFillMail } from "react-icons/ai";
 import { MdLocalOffer } from "react-icons/md";
@@ -7,13 +7,14 @@ import { Spin as Hamburger } from "hamburger-react";
 
 const Navbar = () => {
   let menuHandler = document.querySelector("ul");
-  console.log(menuHandler);
+  let menuRef = useRef();
   const [isOpen, setIsOpen] = useState(false);
   useEffect(() => {
     if (isOpen) {
-      //menuHandler.classList.add("showMenu");
+      menuRef.current.classList.add(classes.showMenu);
+      console.log(classes.showMenu);
     } else {
-      //menuHandler.classList.add("showMenu");
+      menuRef.current.classList.remove(classes.showMenu);
     }
   }, [isOpen]);
 
@@ -21,34 +22,32 @@ const Navbar = () => {
     <nav className={classes.navbar}>
       <div className={classes.logo}>logo</div>
       <Hamburger toggled={isOpen} toggle={setIsOpen} />
-      {isOpen && (
-        <ul className={`${classes.menu} menu`}>
-          <li>
-            <a href="#" className={classes.menuItem}>
-              <AiFillHome className={classes.icon} />
-              Strona główna
-            </a>
-          </li>
-          <li>
-            <a href="#" className={classes.menuItem}>
-              <MdLocalOffer className={classes.icon} />
-              Oferta
-            </a>
-          </li>
-          <li>
-            <a href="#" className={classes.menuItem}>
-              <AiFillMail className={classes.icon} />
-              Kontakt
-            </a>
-          </li>
-          <li>
-            <a href="#" className={classes.menuItem}>
-              <GoAlert className={classes.icon} />
-              Regulamin
-            </a>
-          </li>
-        </ul>
-      )}
+      <ul ref={menuRef} className={`${classes.menu}`}>
+        <li>
+          <a href="#" className={classes.menuItem}>
+            <AiFillHome className={classes.icon} />
+            Strona główna
+          </a>
+        </li>
+        <li>
+          <a href="#" className={classes.menuItem}>
+            <MdLocalOffer className={classes.icon} />
+            Oferta
+          </a>
+        </li>
+        <li>
+          <a href="#" className={classes.menuItem}>
+            <AiFillMail className={classes.icon} />
+            Kontakt
+          </a>
+        </li>
+        <li>
+          <a href="#" className={classes.menuItem}>
+            <GoAlert className={classes.icon} />
+            Regulamin
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
