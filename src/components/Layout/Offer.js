539f34518576a53f9143ec5bb2be206dc254a20b
KubaGlegola <kubaglegola@gmail.com>
2023-01-05 12:20:16 +0100

    added offer section

--- src/components/Layout/Offer.js
+++ src/components/Layout/Offer.js
@@ -1,11 +1,44 @@
 import React from "react";
 import Card from "../UI/Card";
 import classes from "./Offer.module.css";
+import dom from "../../assets/dom.jpg";
+import budowa from "../../assets/budowa.jpeg";
+import elektronarzedzia from "../../assets/elektronarzedzia.jpg";
+import ogrod from "../../assets/ogrod.jpg";
+import Button from "../UI/Button";
 
 const Offer = (props) => {
   return (
     <Card className={classes.offer}>
-      <h1 className={classes.title}>Wyróżnione oferty</h1>
+      <h1 className={classes.title}>
+        W naszej ofercie znajdziesz wszystko czego potrzebujesz!
+      </h1>
+      <div className={classes.imagesContainer}>
+        <div className={classes.imageContainer}>
+          <img src={dom} alt="dom" />
+          <div className={classes.imageButton}>
+            <Button>Dom</Button>
+          </div>
+        </div>
+        <div className={classes.imageContainer}>
+          <img src={budowa} alt="budowa" />
+          <div className={classes.imageButton}>
+            <Button>Budowa</Button>
+          </div>
+        </div>
+        <div className={classes.imageContainer}>
+          <img src={ogrod} alt="ogrod" />
+          <div className={classes.imageButton}>
+            <Button>Ogrod</Button>
+          </div>
+        </div>
+        <div className={classes.imageContainer}>
+          <img src={elektronarzedzia} alt="elektronarzedzia" />
+          <div className={classes.imageButton}>
+            <Button>Elektronarzędzia</Button>
+          </div>
+        </div>
+      </div>
     </Card>
   );
 };
